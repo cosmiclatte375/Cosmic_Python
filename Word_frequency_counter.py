3a32a3060f57ff3c6fc0d6907c937e128af81e90
cosmiclatte375 <cosmiclatte375@gmail.com>
2026-01-25 17:48:55 +0530

    Add files via upload

---
+++ Word_frequency_counter.py
@@ -0,0 +1,21 @@
+user_input=input("Enter an sentence").lower()
+split_sentence=user_input.split(" ")
+dict={}
+
+for i in split_sentence:
+    count=split_sentence.count(i)
+    dict[i]=count
+
+for i,j in dict.items():
+    print(i,":",j)
+
+most_frequent_word=max(dict,key=dict.get)
+
+print(f"total Words: {len(split_sentence)}")
+print(f"Unique words : {len(set(split_sentence))}")
+
+
+
+
+
+
